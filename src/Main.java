48aec509c70c928308534e3298cdc98785f49020
NathanOlgrin <matrica_1997@mail.ru>
2024-06-20 16:33:21 +0300

    Add method compareArrays

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public <T> boolean compareArrays(T[] array1, T[] array2){
+        if(array1 == null || array2 == null || array1.length != array2.length){
+            return false;
+        }
+
+        for (int i = 0; i < array1.length; i++){
+            if(!Objects.equals(array1[i], array2[i])){
+                return false;
+            }
+        }
+        return true;
     }
 }
